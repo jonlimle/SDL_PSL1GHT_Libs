14de9aecd0679c56fadc78bfe821aa32985cc0b3
Marcus Comstedt <marcus@mc.pp.se>
2011-01-30 15:33:56 +0100

    Set CFLAGS for all packages
    Instead of forcing CFLAGS and LDFLAGS to appropriate values in
    scripts/003_SDL_image-1.2.10.sh only, do it in make_SDL_Libs.sh so
    that all packages benefit from it.  Also, no LDFLAGS are actually
    needed anymore...

--- make_SDL_Libs.sh
+++ make_SDL_Libs.sh
@@ -1,2 +1,5 @@
+#!/bin/sh
+CFLAGS="-I$PSL1GHT/target/include/SDL -I$PS3DEV/host/ppu/include/ -I$PS3DEV/host/ppu/include/freetype2"
+export CFLAGS
 for SCRIPT in scripts/*.sh; do "$SCRIPT" || { echo "$SCRIPT: Failed."; exit 1; } done
 
--- scripts/003_SDL_image-1.2.10.sh
+++ scripts/003_SDL_image-1.2.10.sh
@@ -12,8 +12,6 @@ cat ../patches/SDL_image-1.2.10.patch | patch -p1 || { exit 1; }
 	--disable-shared \
 	LIBPNG_CFLAGS="`$PS3DEV/ppu/bin/libpng-config --cflags`" \
 	LIBPNG_LIBS="`$PS3DEV/ppu/bin/libpng-config --libs`" \
-	CFLAGS="-I$PS3DEV/ppu/include" \
-	LDFLAGS="-L$PS3DEV/ppu/lib" \
     	|| { exit 1; }
 
 make && make install || { exit 1; }
