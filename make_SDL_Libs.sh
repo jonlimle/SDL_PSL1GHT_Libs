2b24bff972d9386969263414530997b4aa7b6188
Marcus Comstedt <marcus@mc.pp.se>
2011-01-30 20:56:44 +0100

    Set PKG_CONFIG_PATH

--- make_SDL_Libs.sh
+++ make_SDL_Libs.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 CFLAGS="-I$PSL1GHT/target/include/SDL -I$PS3DEV/host/ppu/include/ -I$PS3DEV/host/ppu/include/freetype2"
+PKG_CONFIG_PATH="$PS3DEV/host/ppu/lib/pkgconfig"
 export CFLAGS
+export PKG_CONFIG_PATH
 for SCRIPT in scripts/*.sh; do "$SCRIPT" || { echo "$SCRIPT: Failed."; exit 1; } done
 
