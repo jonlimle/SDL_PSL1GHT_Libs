2ec764e6e23de1661d9b5dd2366c251f67a9d4f5
Marcus Comstedt <marcus@mc.pp.se>
2011-04-10 13:36:00 +0200

    Fix SDL_net to build 1.2.7 instead of hg snapshot

--- scripts/006_SDL_net.sh
+++ scripts/006_SDL_net.sh
@@ -1,17 +1,17 @@
-#latest release (1.2.7) doesn't compile against SDL 1.3
-#cloning will fail if there is already a cloned repo - don't bork
-hg clone http://hg.libsdl.org/SDL_net/
+wget --continue http://www.libsdl.org/projects/SDL_net/release/SDL_net-1.2.7.tar.gz || { exit 1; }
 
-cd SDL_net || { exit 1; }
+tar xfvz ./SDL_net-1.2.7.tar.gz || { exit 1; }
 
-#let's hope your stars & autotools verions are aligned
-./autogen.sh
+cd SDL_net-1.2.7 || { exit 1; }
+
+cat ../patches/SDL_net-1.2.7.patch | patch -p1 || { exit 1; }
 
 #this is seems like a bug in (the version of) PSL1GHT/ps3toolchain
 # (that I happen to have installed)
-export CFLAGS="$CFLAGS -I$PSL1GHT/target/include"
+CFLAGS="$CFLAGS -I$PSL1GHT/target/include"
+export CFLAGS
 
-./configure --prefix="$PSL1GHT/target" --host=ppc \
+./configure --prefix="$PSL1GHT/target" --host=ppu \
 	--disable-sdltest \
 	--with-sdl-exec-prefix="$PS3DEV/host/ppu" \
 	--disable-shared \
