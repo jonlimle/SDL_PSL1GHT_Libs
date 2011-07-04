813296ecb342bd6944763fba1810ee60a0696271
kraiskil <kraiskil@localhost>
2011-07-04 02:04:19 +0000

    Bugfix: change CFLAGS to reflect SDL install location

--- make_SDL_Libs.sh
+++ make_SDL_Libs.sh
@@ -3,7 +3,7 @@
 SDL_INSTALL_PATH="$PS3DEV/portlibs/ppu/"
 #if you install SDL to a non-default place, add $SDL_INSTALL_PATH/bin to you PATH
 #SDL_INSTALL_PATH="$PS3DEV/portlibs/ppu/sdl-1.2.14/"
-CFLAGS="-I$PSL1GHT/ppu/include/SDL -I$PS3DEV/portlibs/ppu/include/ -I$PS3DEV/portlibs/ppu/include/freetype2 -I$PSL1GHT/ppu/include/"
+CFLAGS="-I$SDL_INSTALL_PATH/include/SDL -I$PS3DEV/portlibs/ppu/include/ -I$PS3DEV/portlibs/ppu/include/freetype2 -I$PSL1GHT/ppu/include/"
 LDFLAGS="-L$SDL_INSTALL_PATH/lib -L$PSL1GHT/ppu/lib -L$PS3DEV/portlibs/ppu/lib -lrt -llv2 -laudio"
 PKG_CONFIG_PATH="$PS3DEV/portlibs/ppu/lib/pkgconfig"
 export CFLAGS
