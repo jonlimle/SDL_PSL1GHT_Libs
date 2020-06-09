813c5158adfeb93c1d38e7853df7ff896e157331
Sergio Padrino <sergio.padrino@gmail.com>
2020-06-09 19:35:19 +0200

    Allow building only specific libraries

--- make_SDL_Libs.sh
+++ make_SDL_Libs.sh
@@ -7,5 +7,33 @@ export LDFLAGS
 export PKG_CONFIG_PATH
 if [ ! -f config.guess ]; then wget --continue http://git.savannah.gnu.org/cgit/config.git/plain/config.guess; fi
 if [ ! -f config.sub ]; then wget --continue http://git.savannah.gnu.org/cgit/config.git/plain/config.sub; fi
-for SCRIPT in scripts/*.sh; do "$SCRIPT" || { echo "$SCRIPT: Failed."; exit 1; } done
 
+## Fetch the build scripts.
+BUILD_SCRIPTS=`ls scripts/*.sh | sort`
+
+## If specific steps were requested...
+if [ $1 ]; then
+
+  ## Find the requested build scripts.
+  REQUESTS=""
+
+  for STEP in $@; do
+    SCRIPT=""
+    for i in $BUILD_SCRIPTS; do
+      if [ `basename $i | cut -d'_' -f1` -eq $STEP ]; then
+        SCRIPT=$i
+        break
+      fi
+    done
+
+    [ -z $SCRIPT ] && { echo "ERROR: unknown step $STEP"; exit 1; }
+
+    REQUESTS="$REQUESTS $SCRIPT"
+  done
+
+  ## Only run the requested build scripts
+  BUILD_SCRIPTS="$REQUESTS"
+fi
+
+## Run the build scripts.
+for SCRIPT in $BUILD_SCRIPTS; do "$SCRIPT" || { echo "$SCRIPT: Failed."; exit 1; } done
